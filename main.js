1de31d0f5bb397251ef75eb9547f7d525c078d89
AlessP93 <alespiscino@libero.it>
2022-04-29 16:27:35 +0200

    aggiunta commenti

---
+++ main.js
@@ -0,0 +1,14 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// Ricordate di scrivere solo il codice JavaScript e non modificare HTML e CSS
+
+
+// crea un evento che fa apparire .hamburger-menu quando viene cliccato l'id #open-menu
+// e poi un altro evento che fa scomparire .hamburger-menu quando viente cliccato l'id #close-menu
+const openMenu = document.getElementById("open-menu");
+openMenu.addEventListener("click",
+    function(){
+        const hamburgerMenu = document.getElementsById("h-menu")
+    }
+);
